e3549efd184a317e045c78ae59bf6d6bff2cd999
ogunakar <62776500+ogunakar@users.noreply.github.com>
2020-04-22 18:21:12 +0300

    add complete version, user input check & restart

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var level = 0;
 
 $(document).keydown(function() {
     if (!started) {
-      
+
       $("#level-title").text("level " + level);
       nextSequence();
       started = true;
@@ -27,12 +27,14 @@ $(".btn").click(function(){
 
   animatePress(userChosenColour);
 
+  checkAnswer(userClickedPattern.length-1);
 });
 
 
 
 function nextSequence () {
 
+  userClickedPattern = [];
   level++;
   $("#level-title").text("level " + level);
 
@@ -42,14 +44,46 @@ function nextSequence () {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
+
 }
 
 
+function checkAnswer(currentLevel) {
+  if ( userClickedPattern[currentLevel] === gamePattern[currentLevel] ) {
 
+      if (userClickedPattern.length === gamePattern.length) {
+
+        setTimeout(function(){
+          nextSequence();
+        }, 1000);
+      }
+  } else {
+
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+
+      setTimeout(function() {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      startOver();
+  };
+}
+
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
 
 function playSound (name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+
 }
 
 
